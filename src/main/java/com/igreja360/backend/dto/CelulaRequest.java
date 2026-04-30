bb21746879d3d5ca009d3507432d99eb4551fbb9
Lebana <33206688+lebanam@users.noreply.github.com>
2026-04-30 18:35:10 -0300

    Add new fields to CelulaRequest DTO

--- src/main/java/com/igreja360/backend/dto/CelulaRequest.java
+++ src/main/java/com/igreja360/backend/dto/CelulaRequest.java
@@ -5,7 +5,9 @@ import java.util.List;
 public class CelulaRequest {
 
     private String nome;
-    private String faixaEtaria;
+    private String tema;
+    private String quando;
+    private String onde;
     private String lider;
     private String coLider;
     private List<Long> membrosIds;
@@ -14,8 +16,16 @@ public class CelulaRequest {
         return nome;
     }
 
-    public String getFaixaEtaria() {
-        return faixaEtaria;
+    public String getTema() {
+        return tema;
+    }
+
+    public String getQuando() {
+        return quando;
+    }
+
+    public String getOnde() {
+        return onde;
     }
 
     public String getLider() {
@@ -34,8 +44,16 @@ public class CelulaRequest {
         this.nome = nome;
     }
 
-    public void setFaixaEtaria(String faixaEtaria) {
-        this.faixaEtaria = faixaEtaria;
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public void setQuando(String quando) {
+        this.quando = quando;
+    }
+
+    public void setOnde(String onde) {
+        this.onde = onde;
     }
 
     public void setLider(String lider) {
