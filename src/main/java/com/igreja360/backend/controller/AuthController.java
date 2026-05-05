9ca7eb7da214d2e33a82391895f7a511c563229f
Lebana <lebanacms@gmail.com>
2026-05-05 17:25:37 -0300

    correçao banco

--- src/main/java/com/igreja360/backend/controller/AuthController.java
+++ src/main/java/com/igreja360/backend/controller/AuthController.java
@@ -19,20 +19,32 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+
+        System.out.println("Tentativa de login");
+        System.out.println("Email recebido: " + request.getEmail());
+        System.out.println("Senha recebida: " + request.getSenha());
+
         Usuario usuario = usuarioRepository.findByEmail(request.getEmail())
                 .orElse(null);
 
         if (usuario == null) {
+            System.out.println("Usuário NÃO encontrado no banco");
             return ResponseEntity.status(401).body("E-mail ou senha inválidos");
         }
+
+        System.out.println("Usuário encontrado: " + usuario.getEmail());
+        System.out.println("Senha banco: " + usuario.getSenha());
 
         if (!usuario.getAtivo()) {
             return ResponseEntity.status(403).body("Usuário inativo");
         }
 
         if (!usuario.getSenha().equals(request.getSenha())) {
+            System.out.println("Senha NÃO bate");
             return ResponseEntity.status(401).body("E-mail ou senha inválidos");
         }
+
+        System.out.println("Login OK");
 
         return ResponseEntity.ok(
                 new LoginResponse(
