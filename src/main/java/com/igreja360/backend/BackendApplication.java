7259708f0521a21ba5485c5ee7bcb198f0219ebe
Lebana <lebanacms@gmail.com>
2026-05-05 18:24:14 -0300

    melhor segurança senha

--- src/main/java/com/igreja360/backend/BackendApplication.java
+++ src/main/java/com/igreja360/backend/BackendApplication.java
@@ -2,6 +2,7 @@ package com.igreja360.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +10,4 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
--- src/main/java/com/igreja360/backend/controller/AuthController.java
+++ src/main/java/com/igreja360/backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.igreja360.backend.dto.LoginResponse;
 import com.igreja360.backend.model.Usuario;
 import com.igreja360.backend.repository.UsuarioRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,39 +13,32 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(UsuarioRepository usuarioRepository) {
+    public AuthController(
+            UsuarioRepository usuarioRepository,
+            PasswordEncoder passwordEncoder
+    ) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-
-        System.out.println("Tentativa de login");
-        System.out.println("Email recebido: " + request.getEmail());
-        System.out.println("Senha recebida: " + request.getSenha());
-
         Usuario usuario = usuarioRepository.findByEmail(request.getEmail())
                 .orElse(null);
 
         if (usuario == null) {
-            System.out.println("Usuário NÃO encontrado no banco");
             return ResponseEntity.status(401).body("E-mail ou senha inválidos");
         }
-
-        System.out.println("Usuário encontrado: " + usuario.getEmail());
-        System.out.println("Senha banco: " + usuario.getSenha());
 
         if (!usuario.getAtivo()) {
             return ResponseEntity.status(403).body("Usuário inativo");
         }
 
-        if (!usuario.getSenha().equals(request.getSenha())) {
-            System.out.println("Senha NÃO bate");
+        if (!passwordEncoder.matches(request.getSenha(), usuario.getSenha())) {
             return ResponseEntity.status(401).body("E-mail ou senha inválidos");
         }
-
-        System.out.println("Login OK");
 
         return ResponseEntity.ok(
                 new LoginResponse(
