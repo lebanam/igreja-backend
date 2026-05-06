c7d1f30c74d456f2c73f50090b201942cb4f0d27
Lebana <lebanacms@gmail.com>
2026-05-05 23:54:04 -0300

    atualização buscar membro

--- src/main/java/com/igreja360/backend/config/SecurityConfig.java
+++ src/main/java/com/igreja360/backend/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 ));
 
                 config.setAllowedMethods(List.of(
-                        "GET", "POST", "PUT", "DELETE", "OPTIONS"
+                        "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
                 ));
 
                 config.setAllowedHeaders(List.of("*"));
